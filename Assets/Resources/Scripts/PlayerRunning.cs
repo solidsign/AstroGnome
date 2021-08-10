a8bd2be67aa00cc70eaf1baa0408e2987561a8a8
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-10 23:07:11 +0300

    Added Running script to Player

---
+++ Assets/Resources/Scripts/PlayerRunning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRunning : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
+    private Vector2 velocity = Vector2.zero;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        velocity.x = Input.GetAxis("Horizontal");
+        velocity.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition((Vector2)transform.position + velocity * Time.fixedDeltaTime);
+    }
+}
