c1ad0b729f7867f8f0da0a085df16f4c6fe03443
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-11 22:36:11 +0300

    added hp and death

---
+++ Assets/Resources/Scripts/ComponentsDisabler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentsDisabler : MonoBehaviour
+{
+    [SerializeField] private List<MonoBehaviour> componentsToDisable;
+
+    public void DisableComponents()
+    {
+        foreach (var component in componentsToDisable)
+        {
+            component.enabled = false;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/HealthHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HealthHandler : MonoBehaviour
+{
+    [SerializeField] private float hp;
+    private ComponentsDisabler disabler;
+    private Animator animator;
+    public float HP => hp;
+
+    private void Start()
+    {
+        disabler = GetComponent<ComponentsDisabler>();
+        animator = GetComponent<Animator>();
+    }
+    public void DealDamage(float damage)
+    {
+        hp -= damage;
+        if(hp <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        disabler.DisableComponents();
+        animator.SetBool("Dead", true);
+    }
+}
