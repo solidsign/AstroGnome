56f8521936c17cf401bdaa22e1b260beea0a515f
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-11 21:45:52 +0300

    Fixed player movement and changed pixel per unit for sprites

--- Assets/Resources/Scripts/PlayerRunning.cs
+++ Assets/Resources/Scripts/PlayerRunning.cs
@@ -5,20 +5,27 @@ public class PlayerRunning : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D rb;
     private Vector2 velocity = Vector2.zero;
-
+    private Animator animator;
+    private bool run;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         velocity.x = Input.GetAxis("Horizontal");
         velocity.y = Input.GetAxis("Vertical");
+        if (run != (velocity.magnitude != 0f))
+        {
+            run = (velocity.magnitude != 0f);
+            animator.SetBool("Run", run);
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition((Vector2)transform.position + velocity * Time.fixedDeltaTime);
+        rb.MovePosition((Vector2)transform.position + speed * Time.fixedDeltaTime *  velocity);
     }
 }
