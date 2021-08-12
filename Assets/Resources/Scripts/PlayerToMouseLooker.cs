709e7263d77ac35e925bf69a1c8f0510e6e2d154
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-12 21:45:22 +0300

    Made player look towards mouse

---
+++ Assets/Resources/Scripts/PlayerToMouseLooker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerToMouseLooker : MonoBehaviour
+{
+    private Camera cam;
+    private bool facingRight = true;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        float mousePosX = cam.ScreenToWorldPoint(Input.mousePosition).x;
+        if ((!facingRight && mousePosX >= transform.position.x) || (facingRight && mousePosX < transform.position.x))
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
