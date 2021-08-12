a809e2504a279562dc14677ba49197a76b3c035b
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-12 18:05:56 +0300

    Created crosshair script

---
+++ Assets/Resources/Scripts/Crosshair.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    [SerializeField] private float distanceFromCenterToAttackPoint;
+    [SerializeField] private Vector3 centerOffset;
+    private Camera cam;
+    private Vector3 mousePos;
+
+    public GameObject pref;
+
+    public Vector3 AttackPoint
+    {
+        get
+        {
+            return (mousePos - (transform.position + centerOffset)).normalized * distanceFromCenterToAttackPoint + transform.position + centerOffset;
+        }
+    }
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Instantiate(pref, AttackPoint, Quaternion.Euler(0, 0, 0));
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position + centerOffset, distanceFromCenterToAttackPoint);
+    }
+}
