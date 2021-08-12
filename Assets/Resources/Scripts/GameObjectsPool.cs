985f4ac9daafe10823b2e825a4c3b39ad3283231
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-12 22:22:06 +0300

    Created gameobjects pool

---
+++ Assets/Resources/Scripts/GameObjectsPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectsPool
+{
+    private Queue<GameObject> objects;
+
+    public GameObjectsPool(int capacity, GameObject prefab)
+    {
+        for (int i = 0; i < capacity; i++)
+        {
+            GameObject obj = Object.Instantiate(prefab);
+            objects.Enqueue(obj);
+            obj.SetActive(false);
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        GameObject obj = objects.Dequeue();
+        objects.Enqueue(obj);
+        return obj;
+    }
+}
