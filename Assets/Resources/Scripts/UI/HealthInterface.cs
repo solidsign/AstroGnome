3a506c21c3eba89d69089f41f0e1b278d65057ef
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-21 22:52:36 +0300

    made health bar with post effects

---
+++ Assets/Resources/Scripts/UI/HealthInterface.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class HealthInterface : MonoBehaviour
+{
+    [SerializeField] private HealthHandler player;
+    [Range(0f, 1f)] [SerializeField] private float maxVignetteIntensity;
+    [Range(0f, 1f)] [SerializeField] private float maxChromaticAberationIntensity;
+    [SerializeField] private float maxHP;
+    [SerializeField] private AnimationCurve intensityCurve;
+    [SerializeField] private float intensityChangeSpeed;
+    private Volume volume;
+    private Vignette vignette;
+    private ChromaticAberration chromaticAberration;
+    void Start()
+    {
+        volume = GetComponent<Volume>();
+        volume.profile.TryGet<Vignette>(out vignette);
+        volume.profile.TryGet<ChromaticAberration>(out chromaticAberration);
+    }
+
+    void Update()
+    {
+        float intensity = intensityCurve.Evaluate(player.HP / maxHP);
+        chromaticAberration.intensity.value = Mathf.Lerp(chromaticAberration.intensity.value, maxChromaticAberationIntensity * intensity, intensityChangeSpeed * Time.deltaTime) ;
+        vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, maxVignetteIntensity * intensity, intensityChangeSpeed * Time.deltaTime);
+    }
+}
