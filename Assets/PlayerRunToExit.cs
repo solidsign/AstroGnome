e2ed5c1566b29497e886e85ccd780deeb3eb95e5
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-27 16:21:46 +0300

    Started making levels

---
+++ Assets/PlayerRunToExit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerRunToExit : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D player;
+    [SerializeField] private ComponentsDisabler playerDisabler;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private Transform holeToJumpIn;
+    [Range(0.4f, 2f)] [SerializeField] private float timeOfRunning;
+
+    public void StartRunning()
+    {
+        playerAnimator.SetBool("Run", true);
+        player.useFullKinematicContacts = false;
+        playerDisabler.DisableComponents();
+    }
+
+    private IEnumerator Running()
+    {
+        Vector3 displacementPerSecond = holeToJumpIn.position - player.transform.position;
+        float timer = 0f;
+        bool jumped = false;
+        while(timer < timeOfRunning)
+        {
+            if(!jumped && timer < 0.5f)
+            {
+                playerAnimator.SetBool("Run", false);
+                playerAnimator.SetTrigger("Jump");
+                jumped = true;
+            }
+            player.MovePosition(player.transform.position + displacementPerSecond * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/MusicHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MusicHandler : MonoBehaviour
+{
+    private AudioSource audioSource; 
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        SetVolume(1f);
+        audioSource.Play();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = Mathf.Clamp01(volume);
+    }
+}
