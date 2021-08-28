f246b564acdee60325a757e877319e790af62dc8
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-28 16:59:12 +0300

    made camera zoom to place where rocket will be and where dwarf will jump in the end of level

---
+++ Assets/CameraZoomer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraZoomer : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve zoomCurve;
+    [SerializeField] private float timeOfZooming;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        StartZooming();
+    }
+
+    public void StartZooming()
+    {
+        StartCoroutine(Zoom());
+    }
+
+    private IEnumerator Zoom()
+    {
+        float timer = 0f;
+        while(timer < timeOfZooming)
+        {
+            cam.orthographicSize = zoomCurve.Evaluate(timer / timeOfZooming);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
--- Assets/Resources/Scripts/PlayerRunToExit.cs
+++ Assets/Resources/Scripts/PlayerRunToExit.cs
@@ -3,22 +3,24 @@ using UnityEngine;
 
 public class PlayerRunToExit : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D player;
+    [SerializeField] private Rigidbody2D playerRB;
     [SerializeField] private ComponentsDisabler playerDisabler;
     [SerializeField] private Animator playerAnimator;
     [SerializeField] private Transform holeToJumpIn;
     [Range(0.4f, 2f)] [SerializeField] private float timeOfRunning;
+    private Transform player;
 
     public void StartRunning()
     {
         playerAnimator.SetBool("Run", true);
-        player.useFullKinematicContacts = false;
+        playerRB.useFullKinematicContacts = false;
         playerDisabler.DisableComponents();
+        player = playerRB.transform;
     }
 
     private IEnumerator Running()
     {
-        Vector3 displacementPerSecond = holeToJumpIn.position - player.transform.position;
+        Vector3 displacementPerSecond = holeToJumpIn.position - player.position;
         float timer = 0f;
         bool jumped = false;
         while(timer < timeOfRunning)
@@ -29,7 +31,7 @@ public class PlayerRunToExit : MonoBehaviour
                 playerAnimator.SetTrigger("Jump");
                 jumped = true;
             }
-            player.MovePosition(player.transform.position + displacementPerSecond * Time.deltaTime);
+            player.Translate(displacementPerSecond * Time.deltaTime);
             yield return null;
         }
     }
