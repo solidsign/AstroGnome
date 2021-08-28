aa88a59af5992c4634a72fc04b433080f932854c
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-28 23:57:41 +0300

    added animations to rocket

--- Assets/Resources/Scripts/Allies/RocketHP.cs
+++ Assets/Resources/Scripts/Allies/RocketHP.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class RocketHP : AllyHealthHandler
 {
+    [SerializeField] private Material material;
+    [SerializeField] private Color baseColor;
+    [SerializeField] private float startIntensity;
+    [SerializeField] private AudioClip hitSound;
+    private AudioSource audioSource;
+    private float intensity;
+    private float intentisyDecreaser;
+
+    protected override void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        intensity = startIntensity;
+        intentisyDecreaser = startIntensity / hp;
+        material.SetColor("_color", baseColor * intensity);
+    }
+
     public override void DealDamage(float damage)
     {
         if (dead) return;
         hp -= damage;
+        intensity -= intentisyDecreaser;
+        material.SetColor("_color", baseColor * intensity);
         if (hp <= 0f)
         {
             // TODO: LOSE();
             return;
         }
+        audioSource.PlayOneShot(hitSound);
     }
 }
---
+++ Assets/RocketFall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RocketFall : MonoBehaviour
+{
+    [SerializeField] private Animator rocketAnimator;
+    [SerializeField] private Transform rocket;
+    [SerializeField] private float longFallDistance;
+    [SerializeField] private float longFallTime;
+    [SerializeField] private float fastFallDistance;
+    [SerializeField] private float fastFallTime;
+
+    public void FallAnimation()
+    {
+        rocketAnimator.SetTrigger("Fall");
+    }
+
+    public void FallLong()
+    {
+        StartCoroutine(Fall(longFallDistance, longFallTime));
+    }
+
+    public void FallFast()
+    {
+        StartCoroutine(Fall(fastFallDistance, fastFallTime));
+    }
+
+    private IEnumerator Fall(float distance, float time)
+    {
+        float timer = 0f;
+        float distancePerSec = distance / time;
+        while(timer < time)
+        {
+            rocket.position = rocket.position + distance * Time.deltaTime * Vector3.up;
+            yield return null;
+        }
+    }
+}
