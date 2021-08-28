e4157da4c2890e135a1c77504b54d02a0f2198f1
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-29 00:54:16 +0300

    started adding scene transitions

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] private Animator curtain;
+    AsyncOperation op;
+
+    private void Start()
+    {
+        curtain.SetTrigger("Down");
+    }
+
+    public void NextLevel()
+    {
+        curtain.SetTrigger("Up");
+        op = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        op.allowSceneActivation = false;
+    }
+
+    public void AllowSceneActivation()
+    {
+        op.allowSceneActivation = true;
+    }
+
+}
