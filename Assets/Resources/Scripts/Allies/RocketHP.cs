d519d689e3ce43662df0a5759423c468b4bf0788
solidsign <49648821+solidsign@users.noreply.github.com>
2021-08-28 22:00:09 +0300

    started making rocket

---
+++ Assets/Resources/Scripts/Allies/RocketHP.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketHP : AllyHealthHandler
+{
+    public override void DealDamage(float damage)
+    {
+        if (dead) return;
+        hp -= damage;
+        if (hp <= 0f)
+        {
+            // TODO: LOSE();
+            return;
+        }
+    }
+}
